b7223ab97a06468301aa4bfdb546daa24d5c28cc
mahesh1901 <mahajanmahesh2001@gmail.com>
2024-03-09 17:39:59 +0530

    Appna_Ro_Finance  Server

---
+++ Appna_Ro_Finance_Server/src/main/java/com/finance/main/AppnaRoFinanceServerApplication.java
@@ -0,0 +1,13 @@
+package com.finance.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppnaRoFinanceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppnaRoFinanceServerApplication.class, args);
+	}
+
+}
