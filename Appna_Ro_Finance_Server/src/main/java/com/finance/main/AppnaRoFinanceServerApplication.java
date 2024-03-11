5d87d0882207f6449e13ed7aaa55dd596f6c4a2b
mahesh1901 <mahajanmahesh2001@gmail.com>
2024-03-11 16:47:29 +0530

    New Changes

--- Appna_Ro_Finance_Server/src/main/java/com/finance/main/AppnaRoFinanceServerApplication.java
+++ Appna_Ro_Finance_Server/src/main/java/com/finance/main/AppnaRoFinanceServerApplication.java
@@ -2,11 +2,14 @@ package com.finance.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AppnaRoFinanceServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Appna_Ro_Finance  Application Server");
 		SpringApplication.run(AppnaRoFinanceServerApplication.class, args);
 	}
 
